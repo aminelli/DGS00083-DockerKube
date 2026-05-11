3b576b67e04adf4182b08fe4a84bcb22d7db7012
aminelli <antonio.minelli@etlforma.com>
2026-05-11 17:29:59 +0200

    upd

--- Esercizi/ES03/mysql.bat
+++ Esercizi/ES03/mysql.bat
@@ -3,4 +3,4 @@
 docker volume create vol-mysql-db
 
 
-docker run -d --name mysql --hostname mysqldb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v vol-mysql-db:/var/lib/mysql mysql:latest 
+docker run -d --name mysqldb --hostname mysqldb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v vol-mysql-db:/var/lib/mysql mysql:latest 
